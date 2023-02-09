66dbeea7a36af2c01ee88c5d766211c2559e68a1
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-09 12:17:10 +0545

    update note

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -7,7 +7,7 @@ class ClassComp extends Component {
     <>
       <h2>Class Components</h2>
       <p>
-        it can maintain some informattions that is private to that Component.{" "}
+        it can maintain some informattions(state) that is private to that Component.{" "}
       </p>
       <p>maintain own data, known as states</p>
       <p>more feature rich</p>
