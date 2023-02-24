9c8c28ac6c42dd22614e30a78eae0560ff3eeacb
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-24 19:45:26 +0545

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
-import Component from './components/Component'
-// import Hello from './components/jsx'
+import "./App.css";
+//jsx
+// import Hello from './components/Jsx'
+// import StyleJsx from "./components/StyleJsx";
+
+// components
+// import Component from './components/Component'
+
+// props
+import PropsFunction, { PropsClass, PropsChild, PropsExpression  } from "./components/Propes";
+
+
 
 function App() {
   return (
     <div className="App">
       <>
-      {/* <Hello/> */}
-      <Component />
+        {/* <Hello/> */}
+        {/* <StyleJsx /> */}
+        {/* <Component /> */}
+
+        <PropsFunction type="function" rate="good"/>
+        {/* props */}
+        {/* 
+        <PropsClass/>
+        <PropsChild>
+          <button>this is a button, which is added from app.js file</button>
+        </PropsChild> 
+      */}
+      <PropsExpression math={2*2} />
       </>
     </div>
   );
