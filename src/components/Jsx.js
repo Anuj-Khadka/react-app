f4e0f9a473186ca67ef38cffa14fac1f641b0764
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-24 19:44:52 +0545

    update jsx with expressions

--- src/components/Jsx.js
+++ src/components/Jsx.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 
-function Hello(){
-    return (
-        <>
-        <h1 
-        className='title' 
-        style={{color: 'red'}}>
-            Welcome!
-        </h1>
-        <p>Today we are just revising what we have learnt yesterday.</p>
-        </>
+const user = (userName) => {
+  return userName.toLowerCase() === "anuj" ? "yes" : "no";
+};
 
-        // the react way  (raw react code)        
-        // React.createElement(
-        //     'div',
-        //     {id: 'old-way', className:'older-way'},
-        //     React.createElement(
-        //         'h1',
-        //         null,
-        //         'Welcome!'
-        //     )
-        //     )
-    )
+function Hello() {
+  return (
+    <>
+      <h1 className="title" style={{ color: "red" }}>
+        Welcome!
+      </h1>
+      <p>Today we are just revising what we have learnt yesterday.</p>
+      <p>{user("anUj")}</p>
+    </>
+
+    // the react way  (raw react code)
+    // React.createElement(
+    //     'div',
+    //     {id: 'old-way', className:'older-way'},
+    //     React.createElement(
+    //         'h1',
+    //         null,
+    //         'Welcome!'
+    //     )
+    //     )
+  );
 }
 
 export default Hello;
