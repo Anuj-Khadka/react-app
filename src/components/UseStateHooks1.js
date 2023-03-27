1dbf7ca768995b0806d3de31cc3790412808bb5f
Anuj-Khadka <anujkhadka008@gmail.com>
2023-03-27 22:38:04 +0545

    update useState

--- src/components/UseStateHooks1.js
+++ src/components/UseStateHooks1.js
@@ -9,6 +9,14 @@ const UseStateHooks = () => {
       <br />
       useState with previous state
       <UseStatePrevStateCounter />
+      <br />
+      <br />
+      useState with object
+      <UseStateObject />
+      <br />
+      <br />
+      useState with array
+      <UseStateArray />
     </div>
   );
 };
@@ -48,5 +56,80 @@ function UseStatePrevStateCounter() {
   );
 }
 
+// useState with object
+
+function UseStateObject() {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  return (
+    <div>
+      <form action="">
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          value={name.firstName}
+          onChange={(e) =>
+            setName({
+              ...name,
+              firstName: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          value={name.lastName}
+          onChange={(e) =>
+            setName({
+              ...name,
+              lastName: e.target.value,
+            })
+          }
+        />
+        <h2>firstname: {name.firstName}</h2>
+        <h2>lastname: {name.lastName}</h2>
+        <h2>{JSON.stringify(name)}</h2>
+      </form>
+    </div>
+  );
+}
+
+// useState with array
+
+function UseStateArray() {
+  const [items, setItems] = useState([]);
+
+  const clickHandler = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 10) + 1,
+      },
+    ]);
+  };
+  return (
+    <div>
+      <button onClick={clickHandler}>Clickme</button>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.id} --------- {item.value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default UseStateHooks;
-export { UseStateCounter, UseStatePrevStateCounter };
+export {
+  UseStateCounter,
+  UseStatePrevStateCounter,
+  UseStateObject,
+  UseStateArray,
+};
