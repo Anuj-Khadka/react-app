e6df962cca120b8fbe48cdd873ec189ec709c56e
Anuj-Khadka <anujkhadka008@gmail.com>
2023-03-28 22:02:19 +0545

    update useEffect

--- src/components/UseEffectHooks1.js
+++ src/components/UseEffectHooks1.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function UseEffectHooks() {
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    document.title = `clicked ${count} times`
+  })
+
   return (
     <div>
-        hello workd
+      <button onClick={setCount((count) => count + 1)}>
+        clicked {count} times
+      </button>
     </div>
-  )
+  );
 }
+
 
 export default UseEffectHooks;
