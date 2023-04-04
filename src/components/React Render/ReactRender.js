9b6b84be22f4c8b2905fdd4c81a4e7988832292c
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-04 22:36:20 +0545

    update react render

--- src/components/React Render/ReactRender.js
+++ src/components/React Render/ReactRender.js
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  ChildA,
+  ContextAndMemoParent,
+} from "./Incorrect Memo Optimization/ContextAndMemo";
+import IncorrectWithPropsReference from "./Incorrect Memo Optimization/IncorrectWithPropsReference";
+import IncorrectWithImpureComponent from "./Incorrect Memo Optimization/IncorrectWithImpureComponent";
+import IncorrectWithChildrenParent from "./Incorrect Memo Optimization/IncorrectWithChildren";
+import ReactMemoParent from "./ReactMemo";
 import Parent from "./ParentChildComponents";
 import StateImmutibility from "./StateImmutibility";
 import UseReducer from "./UseReducer";
@@ -21,11 +29,36 @@ const ReactRender = () => {
       <br />
       State Immutability with useState and useReducer
       <StateImmutibility /> 
-      */}
       <br />
       <br />
       Parent Child Rendering
       <Parent />
+      <br />
+      <br />
+      React Memo Rendering
+      <ReactMemoParent />
+      <br />
+      <br />
+      Incorrect Memo With Children Rendering
+      <IncorrectWithChildrenParent />
+      <br />
+      <br />
+      Incorrect Memo With Impure Component Rendering
+      <IncorrectWithImpureComponent />
+      <br />
+      <br />
+      Incorrect Memo With Impure Component Rendering
+      <IncorrectWithPropsReference />
+      */}
+      <br />
+      <br />
+      React Context Rendering
+      {/* <ContextAndMemoParent /> */}
+      another way to optimize Context rendering
+      <ContextAndMemoParent>
+        {" "}
+        <ChildA />
+      </ContextAndMemoParent>
     </div>
   );
 };
