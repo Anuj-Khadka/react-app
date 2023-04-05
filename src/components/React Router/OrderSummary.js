4432736c44ca6532ad0b9d4cc2775356059ac499
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-05 22:22:14 +0545

    navigating programmatically

---
+++ src/components/React Router/OrderSummary.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const OrderSummary = () => {
+    const navigate = useNavigate()
+  return (
+    <div>
+      Order Confirmed!
+      <div>
+        <button onClick={()=>navigate(-1)}>Go Back</button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
