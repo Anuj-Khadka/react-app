5f654a43ae28fb9bc075e9d069a70ec924c479a4
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-10 22:16:51 +0545

    update react-redux

--- src/components/React Redux/redux/store.js
+++ src/components/React Redux/redux/store.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import cakeReducer from "./cake/cake-reducer";
+import rootReducer from "./root-reducer";
 
-const store = createStore(cakeReducer);
+import logger from "redux-logger";
+
+// const store = createStore(cakeReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
