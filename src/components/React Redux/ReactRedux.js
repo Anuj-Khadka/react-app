cfa570bd3b61b67dc2afb23f63d0ff8fbb54ae97
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-10 22:16:09 +0545

    update react-redux

--- src/components/React Redux/ReactRedux.js
+++ src/components/React Redux/ReactRedux.js
@@ -2,12 +2,28 @@ import React from "react";
 import CakeContainer from "./CakeContainer";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import HooksCakeContainer from "./HooksCakeContainer";
+import IceCreamContainer from "./redux/IceCreamContainer";
+import NewCakeContainer from "./redux/NewCakeContainer";
 
 const ReactRedux = () => {
   return (
     <Provider store={store}>
       <div>
+        normal cake container
         <CakeContainer />
+        hook cake container
+        <HooksCakeContainer />
+        <br />
+        <br />
+        <br />
+        buy icecream
+        <IceCreamContainer />
+        <br />
+        <br />
+        <br />
+        new Cake Container with payload
+        <NewCakeContainer />
       </div>
     </Provider>
   );
