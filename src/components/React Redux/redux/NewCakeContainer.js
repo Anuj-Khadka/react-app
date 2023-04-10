2c73e5b84dcf06df126cfb6e3b96119708582004
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-10 22:16:45 +0545

    update react-redux

---
+++ src/components/React Redux/redux/NewCakeContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import buyCake from "./cake/cake-actions";
+
+const NewCakeContainer = (props) => {
+  const [number, setNumber] = useState(1);
+  return (
+    <div>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <h2>Number of Cakes = {props.numOfCakes}</h2>
+      <button onClick={props.buyCake(number)}>Buy {number} Cakes</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.cake.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer);
