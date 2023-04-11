33b04c3b51020c5ffa93da1fcc4f2b9cf3ba0cd6
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-11 22:07:05 +0545

    update react hook

---
+++ src/components/hooks/ReactHooks.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+// hooks
+// import UseStateHooks from "./UseStateHooks1";
+// import UseEffectHooks from "./UseEffectHooks1";
+// import UseEffectHook from "./UseEffectHook2";
+// import UseContextHook from "./UseContextHook";
+// import UseReducerHook1 from "./UseReducerHook1";
+// import UseReducerHook2 from "./UseReducerHook2";
+// import UseCallbackHook from "./UseCallbackHook";
+// import UseMemoHook from "./UseMemoHook";
+// import UseRefHook from "./UseRefHook";
+import CustomHooks from "./CustomHooks";
+
+const ReactHooks = () => {
+  return (
+    <div>
+      {/* hooks */}
+      {/* <UseStateHooks /> */}
+      {/* <UseEffectHooks /> */}
+      {/* <UseEffectHook /> */}
+      {/* <UseContextHook /> */}
+      {/* <UseReducerHook1 /> */}
+      {/* <UseReducerHook2 /> */}
+      {/* <UseCallbackHook /> */}
+      {/* <UseMemoHook /> */}
+      {/* <UseRefHook /> */}
+      <CustomHooks />
+    </div>
+  );
+};
+
+export default ReactHooks;
