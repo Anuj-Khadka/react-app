055219809e0d0fa1c8d4a0170c5570dcc2a07a5a
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-11 22:06:14 +0545

    update react-redux

--- src/components/React Redux/redux/cake/cake-reducer.js
+++ src/components/React Redux/redux/cake/cake-reducer.js
@@ -4,11 +4,11 @@ const initialState = {
   numOfCakes: 10,
 };
 
-const cakeReducer = (state= initialState, action) => {
+const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
-        numOfCakes: state.numOfCakes - 1,
+        numOfCakes: state.numOfCakes - action.payload,
       };
     default:
       return state;
