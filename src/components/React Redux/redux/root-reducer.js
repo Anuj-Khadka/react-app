c8653d5b74eb69b82516d71d2518a0ced7afa077
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-12 21:05:18 +0545

    update redux-react

--- src/components/React Redux/redux/root-reducer.js
+++ src/components/React Redux/redux/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import iceCreamReducer from "./icecream/icecream-reducer";
 import cakeReducer from "./cake/cake-reducer";
+import fetchReducer from "./user/user-reducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  users: fetchReducer
 });
 
 export default rootReducer;
