3d68de9da18603749de45cbfd719a158be167963
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-12 21:03:56 +0545

    updare redux-react

--- src/components/React Redux/ReactRedux.js
+++ src/components/React Redux/ReactRedux.js
@@ -5,12 +5,14 @@ import { Provider } from "react-redux";
 import HooksCakeContainer from "./HooksCakeContainer";
 import IceCreamContainer from "./redux/IceCreamContainer";
 import NewCakeContainer from "./redux/NewCakeContainer";
+import ItemContainer from "./redux/ItemContainer";
+import UserContainer from "./UserContainer";
 
 const ReactRedux = () => {
   return (
     <Provider store={store}>
       <div>
-        normal cake container
+        {/* normal cake container
         <CakeContainer />
         hook cake container
         <HooksCakeContainer />
@@ -24,6 +26,16 @@ const ReactRedux = () => {
         <br />
         new Cake Container with payload
         <NewCakeContainer />
+        <br />
+        <br />
+        <br />
+        mapstatetoprops for Cake
+        <ItemContainer cake />
+        <br />
+        mapstatetoprops for icecream
+        <ItemContainer icecream/> */}
+
+        <UserContainer />
       </div>
     </Provider>
   );
