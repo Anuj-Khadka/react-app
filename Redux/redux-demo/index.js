8a7b750d0afb270392b918e7eac550ff908d2f7d
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-13 23:07:16 +0545

    update react-redux

--- Redux/redux-demo/index.js
+++ Redux/redux-demo/index.js
@@ -5,15 +5,23 @@
 const redux = require("redux");
 const creatingStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const bindactionCreators = redux.bindActionCreators
 
 // action
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
+const RESTOCK_CAKE = "RESTOCK_CAKE";
 
 const buyCake = () => {
   return {
     type: BUY_CAKE,
     info: "first redux application",
+  };
+};
+const restockCake = (qty=1) => {
+  return {
+    type: RESTOCK_CAKE,
+    payload: qty,
   };
 };
 const buyIceCream = () => {
@@ -29,10 +37,12 @@ const initialState = {
   numOfIceCream: 50,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case "BUY_CAKE":
       return { ...state, numOfCakes: state.numOfCakes - 1 };
+    case RESTOCK_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes + action.payload};
     case "BUY_ICECREAM":
       return { ...state, numOfIceCream: state.numOfIceCream - 1 };
     default:
@@ -42,6 +52,7 @@ const reducer = (state = initialState, action) => {
 */
 
 // multiple reducers
+// /*
 const initialCakeState = {
   numOfCakes: 10,
 };
@@ -65,14 +76,25 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
       return state;
   }
 };
+// */
 
 // store
 // conbine multiple reducers
-const rootReducer  = combineReducers({
+const rootReducer = combineReducers({
   cake: cakeReducer,
-  iceCream: iceCreamReducer
-})
+  iceCream: iceCreamReducer,
+});
 const store = creatingStore(rootReducer);
+
+// bindActionCreators
+/*
+const action = bindactionCreators({buyCake, restockCake}, store.dispatch)
+action.buyCake()
+action.buyCake()
+action.buyCake()
+action.restockCake()
+*/
+
 console.log("initial state", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("updated state", store.getState());
@@ -83,5 +105,8 @@ store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
 store.dispatch(buyCake());
-console.log(store.getState().cake)
+store.dispatch(restockCake(2));
+store.dispatch(buyCake());
+console.log(store.getState().cake);
 unsubscribe();
+
