c0209adba4fe7277ef368bcf568cf9902d8a6289
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-14 22:19:50 +0545

    update reduxjs/toolkit

--- Redux/rtk-demo/app/store.js
+++ Redux/rtk-demo/app/store.js
@@ -1,11 +1,20 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
+// middleware
+// const reduxLogger = require("redux-logger");
+
 const cakeReducer = require("../features/cake/cakeSlice");
+const iceCreamReducer = require("../features/icecream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice")
+
+// const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
+    iceCream: iceCreamReducer,
+    user: userReducer,
   },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-
-module.exports = store
+module.exports = store;
