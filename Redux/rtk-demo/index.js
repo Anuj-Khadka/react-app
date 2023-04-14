f02608d25fc5e2f0a3b88d57298360c7992aa0a6
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-14 22:19:29 +0545

    update reduxjs/toolkit

--- Redux/rtk-demo/index.js
+++ Redux/rtk-demo/index.js
@@ -1,5 +1,8 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./features/icecream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("initial state", store.getState());
 
@@ -7,10 +10,14 @@ const unsubscribe = store.subscribe(() => {
   console.log("updated state", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(2));
-store.dispatch(cakeActions.restocked(1));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(cakeActions.restocked(2));
+// store.dispatch(cakeActions.restocked(1));
+// store.dispatch(iceCreamActions.restocked(1));
+store.dispatch(fetchUsers());
 
-unsubscribe();
+// unsubscribe();
