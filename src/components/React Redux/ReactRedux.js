c7173a8c690c9f85b2a13285b96788adc98cba91
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-14 22:21:00 +0545

    update reduxjs/toolkit

--- src/components/React Redux/ReactRedux.js
+++ src/components/React Redux/ReactRedux.js
@@ -1,41 +1,55 @@
 import React from "react";
-import CakeContainer from "./CakeContainer";
-import store from "./redux/store";
+import store from "./rtk/app/store";
 import { Provider } from "react-redux";
-import HooksCakeContainer from "./HooksCakeContainer";
-import IceCreamContainer from "./redux/IceCreamContainer";
-import NewCakeContainer from "./redux/NewCakeContainer";
-import ItemContainer from "./redux/ItemContainer";
-import UserContainer from "./UserContainer";
+
+// react-redux
+// import CakeContainer from "./CakeContainer";
+// import HooksCakeContainer from "./HooksCakeContainer";
+// import IceCreamContainer from "./redux/IceCreamContainer";
+// import NewCakeContainer from "./redux/NewCakeContainer";
+// import ItemContainer from "./redux/ItemContainer";
+// import UserContainer from "./UserContainer";
+
+// @reduxjs/toolkit
+import CakeView from "./rtk/features/cake/CakeView";
+import IcecreamView from "./rtk/features/icecream/IcecreamView";
+import UserView from "./rtk/features/user/UserView";
 
 const ReactRedux = () => {
   return (
     <Provider store={store}>
       <div>
+        {/* normal react-redux */}
         {/* normal cake container
-        <CakeContainer />
-        hook cake container
-        <HooksCakeContainer />
-        <br />
-        <br />
-        <br />
-        buy icecream
-        <IceCreamContainer />
-        <br />
-        <br />
-        <br />
-        new Cake Container with payload
-        <NewCakeContainer />
-        <br />
-        <br />
-        <br />
-        mapstatetoprops for Cake
-        <ItemContainer cake />
-        <br />
-        mapstatetoprops for icecream
-        <ItemContainer icecream/> */}
+    //     <CakeContainer />
+    //     hook cake container
+    //     <HooksCakeContainer />
+    //     <br />
+    //     <br />
+    //     <br />
+    //     buy icecream
+    //     <IceCreamContainer />
+    //     <br />
+    //     <br />
+    //     <br />
+    //     new Cake Container with payload
+    //     <NewCakeContainer />
+    //     <br />
+    //     <br />
+    //     <br />
+    //     mapstatetoprops for Cake
+    //     <ItemContainer cake />
+    //     <br />
+    //     mapstatetoprops for icecream
+    //     <ItemContainer icecream/> 
+    //     <UserContainer />
+    */}
 
-        <UserContainer />
+
+        {/* @reduxjs/toolkit */}
+        <CakeView />
+        <IcecreamView />
+        <UserView />
       </div>
     </Provider>
   );
