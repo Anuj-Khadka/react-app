97986a2623240f7cbc493df779fb6ac187a8500c
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-15 22:54:57 +0545

    add countup

---
+++ src/components/React Practical/CountUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CountUp = () => {
+  return (
+    <div>
+      <h2>
+        <a href="https://github.com/glleyresy/react-countup">CountUp</a>
+      </h2>
+    </div>
+  );
+};
+
+export default CountUp;
