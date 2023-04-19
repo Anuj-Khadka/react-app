69b0cc13ef50e73d59df897abaa10dd98bb9f4ff
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-19 22:02:27 +0545

    update

---
+++ src/components/advanced/RenderProps.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class DemoRenderProps extends Component {
+  render() {
+    return <h1>{this.props.render(false)}</h1>;
+  }
+}
+
+class CounterRenderProps extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  countIncrement = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    });
+  };
+
+  render() {
+    return (
+    //   <div>{this.props.render(this.state.count, this.countIncrement)}</div>
+    // another way 
+      <div>{this.props.children(this.state.count, this.countIncrement)}</div>
+    );
+  }
+}
+
+class ClickCounterRenderProps extends Component {
+  render() {
+    const { count, countIncrement } = this.props;
+    return (
+      <div>
+        <button onClick={countIncrement}>
+          {this.props.yourName} Clicked {count} times
+        </button>
+      </div>
+    );
+  }
+}
+
+class HoverCounterRenderProps extends Component {
+  render() {
+    const { count, countIncrement } = this.props;
+    return (
+      <div>
+        <h2 onMouseOver={countIncrement}>hovered {count} times</h2>
+      </div>
+    );
+  }
+}
+
+export default DemoRenderProps;
+export { CounterRenderProps, ClickCounterRenderProps, HoverCounterRenderProps };
